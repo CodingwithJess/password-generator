e7025986b758f31449e5ed3ea9cd939e8cd9516b
CodingwithJess <thejessiebess@gmail.com>
2020-06-26 17:02:32 -0600

    added password option prompts to JS code

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split ("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split ("");
+var numbers = "1234567890".split ("");
+var special = "!@#$%&*?".split("");
+
+// var userPassword = [...lowerCase, ...upperCase, ...numbers, ...special]
+function passwordOptions (){
+  var length = prompt("Choose a length between 8 and 128 characters:")
+  if (isNaN(length)=== true ){
+    alert("Please enter a number.")
+    return
+  }
+  if (length<8||length>128){
+    alert("Please enter a number between 8 and 128")
+    return
+  }
+
+  var specChar = confirm("Click ok to include special characters.")
+  var confNum = confirm("Click ok to include numbers.")
+  var confLower = confirm("Click ok to include lower case letters.")
+  var confUpper = confirm("Click ok to include UPPER case letters.")
+
+  if (specChar===false&&confNum===false&&confLower===false&&confUpper===false){
+    alert("Must select at least one character type. Try again.")
+    return
+  }
+}
+
+passwordOptions()
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
